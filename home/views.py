48f0749ee91b6114a81417691b480e6c603d0323
venhha123 <venh.ha@gmail.com>
2022-11-27 13:17:52 +0700

    update

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+   response = HttpResponse()
+   response.writelines('<h1>Xin chào</h1>')
+   response.write('Đây là app home')
+   return response
