9df50a4b9739ffbbafde5bfe909421b5068ea069
venhha123 <venh.ha@gmail.com>
2022-11-27 16:33:12 +0700

    update

--- webapp_proj/settings.py
+++ webapp_proj/settings.py
@@ -118,7 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
